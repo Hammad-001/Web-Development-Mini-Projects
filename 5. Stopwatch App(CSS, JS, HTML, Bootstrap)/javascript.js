37d8c94b22d8bba75b720fe65f080a21b783d8fa
Hammad-001 <hammadfaisal178@gmail.com>
2021-09-16 12:36:46 +0500

    Background Image Updated

---
+++ 5. Stopwatch App(CSS, JS, HTML, Bootstrap)/javascript.js
@@ -0,0 +1,9 @@
+$(function() {
+    var mode = false; //App mode Running or Not
+    var timeCounter = 0; //Counts Time
+    var lapCounter = 0; //Counts Lap Time
+    var actionRun; // to start the loop
+    var numberOfLaps = 0;
+
+
+});
